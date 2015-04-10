fe327a54e6fffcad81b3311abbec5dbaceae8e73
David Duarte <david.duarte@tecnico.ulisboa.pt>
2015-04-10 21:24:39 +0100

    Add Subscribe Dialog layout

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/fragment/SubscribeWorkspaceFragment.java
@@ -0,0 +1,21 @@
+package pt.ulisboa.tecnico.cmov.airdesk.fragment;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SubscribeWorkspaceFragment extends DialogFragment {
+
+    public SubscribeWorkspaceFragment() {
+        // Empty constructor required for DialogFragment
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
